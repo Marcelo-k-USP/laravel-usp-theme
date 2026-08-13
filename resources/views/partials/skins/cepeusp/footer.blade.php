ba192872c76ed7a641cefcbec077e83741a87a0e
Fernando <fernando.td.moraes@gmail.com>
2026-08-13 18:11:42 -0300

    Adiciona skin cepeusp

---
+++ resources/views/partials/skins/cepeusp/footer.blade.php
@@ -0,0 +1,9 @@
+@section('skin_footer')
+<!-- Footer cepeusp -->
+<footer>
+    <div class="text-center py-4">
+        <hr>
+        <a href="https://cepe.usp.br/" target="_blank">© {{ now()->year }} CEPEUSP</a>
+    </div>
+</footer>
+@endsection
